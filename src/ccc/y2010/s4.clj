72ffdb0cb247c651dff40dfd032782d1d0bec96e
Mitchell Kember <mk12360@gmail.com>
2013-12-10 21:05:32 -0500

    Started 2010 S4 (animal farm)

---
+++ src/ccc/y2010/s4.clj
@@ -0,0 +1,7 @@
+;;; Copyright 2013 Mitchell Kember. Subject to the MIT License.
+;;; 2010 Canadian Computing Competition: Senior Division
+;;; Problem S4: Animal Farm
+
+(ns ccc.y2010.s4)
+
+; No clue how to do this
